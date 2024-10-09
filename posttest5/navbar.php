cc3ad41b34a0af2b110231307ac41cd1a7f30307
Arno <128877663+CoderM3@users.noreply.github.com>
2024-10-09 21:37:09 +0800

    Add files via upload

---
+++ posttest5/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar">
+        <img class="logo" src="pngwing.com.png" alt="logohks">
+        <ul>
+            <li><a href="#Scratch">Scratch & Dents Deals</a></li>
+            <li><a href="aboutme.php">About Me</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#wheels">Shop Wheels</a></li>
+            <li><a href="login.php">Login/Sign up</a></li>
+            <li><button class ="darkie" onclick="darkmode()">Toggle dark mode</button></li>
+        </ul>
+        <button class="hamburger" id="hamburger">
+          <i class="fa fa-bars"></i>
+        </button>
+    </nav>
